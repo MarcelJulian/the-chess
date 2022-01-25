369c5428ff727b6bc69bdf86664a0d6bf91df49e
choirunnisaptr <choirunnisappang@gmail.com>
2022-01-25 22:43:24 +0700

    update main app

--- src/main/java/com/thesis/TheChess/TheChess.java
+++ src/main/java/com/thesis/TheChess/TheChess.java
@@ -2,7 +2,13 @@ package com.thesis.TheChess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.thesis.TheChess"})
+//@ComponentScan(basePackages = {"com.thesis.TheChess.controller", "com.thesis.TheChess.model", "com.thesis.TheChess.repository", "com.thesis.TheChess.service"})
+@EntityScan("com.thesis.TheChess.*")
+//@EntityScan("com.thesis.TheChess.model")
 @SpringBootApplication
 public class TheChess {
 
