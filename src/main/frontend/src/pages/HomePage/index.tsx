ec4e7ca82f981559a3d907f356bc6662a4632b8c
verinaarmelia <verinaarmelia@gmail.com>
2022-01-30 03:10:07 +0700

    HomePage (Without Signin)

--- src/main/frontend/src/pages/HomePage/index.tsx
+++ src/main/frontend/src/pages/HomePage/index.tsx
@@ -6,7 +6,17 @@ import {
   ButtonGroup,
   Container,
   Grid,
-  GridItem
+  GridItem,
+  Slider,
+  Flex,
+  // Spacer,
+  Center,
+  Text,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Stack,
+  Switch
 } from "@chakra-ui/react";
 
 import NavBar from "components/NavBar";
@@ -17,20 +27,55 @@ function HomePage() {
       <NavBar />
       <Container />
       <Container>
-        <Box border="1px" borderColor="gray.200" borderRadius="lg" h="2rem">
-          <p>asd</p>
+        {/* Container Atas */}
+        <Box border="2px" borderColor="gray.500" borderRadius="lg" h="6rem">
+          <GridItem w="100%" h="8" bg="transparent">
+            <Center w="500px" h="40px" bg="transparent">
+              <Text fontSize={20}>Play With</Text>
+            </Center>
+          </GridItem>
+          {/* Play with atas */}
+
+          {/* Grid Tengah */}
+          <Grid templateColumns="repeat(2, 0fr)" gap={0}>
+            <GridItem colSpan={2} w="250px" h="60px" bg="transparent">
+              <Center w="250px" h="60px" bg="transparent">
+                <Button variant="ghost" w="220px" h="50px">
+                  Bot
+                </Button>
+              </Center>
+            </GridItem>
+
+            <GridItem
+              colStart={3}
+              colEnd={5}
+              w="240px"
+              h="50px"
+              bg="transparent"
+            >
+              <Center w="230px" h="60px" bg="transparent">
+                <Button variant="ghost" w="220px" h="50px">
+                  Human
+                </Button>
+              </Center>
+            </GridItem>
+          </Grid>
         </Box>
-        <Box border="1px" borderColor="gray.200" borderRadius="lg" h="2rem">
-          <p>asd</p>
-        </Box>
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+
+        {/* Container Bawah */}
         <Grid
           border="1px"
-          borderColor="gray.200"
+          borderColor="gray.800"
           borderRadius="lg"
           templateRows="repeat(4, 1fr)"
           templateColumns="repeat(2, 1fr)"
-          gap={4}
+          gap={0}
         >
+          {/* Box Strength Level AI */}
           <GridItem px="1rem" py="0.5rem">
             <Box>Strength</Box>
             <ButtonGroup spacing="0" size="sm">
@@ -60,12 +105,70 @@ function HomePage() {
               </Button>
             </ButtonGroup>
           </GridItem>
-          <GridItem w="100%" h="10" bg="blue.500" />
-          <GridItem w="100%" h="10" bg="blue.500" />
-          <GridItem w="100%" h="10" bg="blue.500" />
-          <GridItem w="100%" h="10" bg="blue.500" />
-          <GridItem w="100%" h="10" bg="blue.500" />
-          <GridItem w="100%" h="10" bg="blue.500" colSpan={2} />
+          {/* Box color */}
+          <GridItem w="100%" h="10" py="0.5rem">
+            <Box h="2rem">Color</Box>
+            <Flex>
+              <Button class="btnBlack" py="1rem" m="0" />
+              <Button class="btnWhite" py="0" m="0" />
+              <Button class="btnBlack" py="0" m="0" />
+            </Flex>
+          </GridItem>
+          {/* Box Time Control */}
+          <GridItem w="100%" h="10" px="1rem" py="1rem">
+            <Box>Time Control</Box>
+          </GridItem>
+          {/* Box Toggle */}
+          <GridItem w="100%" h="5" bg="transparent" py="1rem">
+            <Grid templateColumns="repeat(3, 1fr)" gap={0}>
+              <GridItem w="100%" h="10" bg="transparen">
+                <Text fontSize={13}>Unlimited</Text>
+              </GridItem>
+              <GridItem w="100%" px="10px" h="10" bg="transparen">
+                <Stack direction="row">
+                  <Switch colorScheme="teal" size="md" />
+                </Stack>
+              </GridItem>
+              <GridItem w="100%" h="10" bg="transparen">
+                <Text fontSize={13}>Real-Time</Text>
+              </GridItem>
+            </Grid>
+          </GridItem>
+
+          {/* Box Minute Per Side */}
+          <GridItem w="100%" h="1" bg="transparent">
+            <Box px="1rem" py="0.5rem">
+              Minute Per Side :{" "}
+            </Box>
+            <Box paddingLeft={5} paddingRight={58}>
+              <Slider aria-label="slider-ex-1" defaultValue={10}>
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </Box>
+          </GridItem>
+
+          {/* Box Minute Per Side */}
+          <GridItem w="100%" h="1" bg="transparent">
+            <Box py="0.5rem">Increment in second : </Box>
+            <Box paddingRight={5}>
+              <Slider aria-label="slider-ex-1" defaultValue={10}>
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </Box>
+          </GridItem>
+
+          {/* Box START */}
+          <GridItem w="100%" h="10" bg="transparent" colSpan={2}>
+            <Center>
+              <Button colorScheme="blue">Start</Button>
+            </Center>
+          </GridItem>
         </Grid>
       </Container>
     </Box>
