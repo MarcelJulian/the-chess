8750d1f758f65c3678baa8b6966348e1d9d37f5d
MarcelJulian <marceljulian2047@gmail.com>
2022-02-12 20:51:46 +0700

    refactor: :recycle: refactor to using material ui

--- src/main/frontend/src/pages/HumanPage/index.jsx
+++ src/main/frontend/src/pages/HumanPage/index.jsx
@@ -1,210 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Container,
-  Grid,
-  GridItem,
-  Slider,
-  Flex,
-  // Spacer,
-  Center,
-  Text,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  Stack,
-  Switch
-} from "@chakra-ui/react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+function TimeControlButton({ timeControl }) {
+  return (
+    <Button
+      variant="contained"
+      size="large"
+      sx={{
+        width: "100%",
+        minHeight: "8rem",
+        fontSize: "2rem"
+      }}
+    >
+      {timeControl}
+    </Button>
+  );
+}
 
 function HumanPage() {
-  // replace with store
-  const [isLogin, setIsLogin] = useState(false);
-  const [botStrength, setBotStrength] = useState(1);
-  const [botColor, setBotColor] = useState("random");
-  const [botTimeControlMode, setBotTimeControlMode] = useState("unlimited");
-  const [botTimeControlMinute, setBotTimeControlMinute] = useState(10);
-  const [botTimeControlIncrement, setBotTimeControlIncrement] = useState(0);
+  const timeControlButtons = [
+    <TimeControlButton timeControl="5+3" />,
+    <TimeControlButton timeControl="10+0" />,
+    <TimeControlButton timeControl="10+5" />,
+    <TimeControlButton timeControl="15+10" />,
+    <TimeControlButton timeControl="30+0" />,
+    <TimeControlButton timeControl="30+20" />
+  ];
 
-  const generateBotStrengthGroupButton = () => {
-    const buttons = [];
-
-    for (let i = 1; i < 9; i++) {
-      buttons.push(
-        <Button
-          borderLeftRadius={i === 1 ? "md" : "none"}
-          borderRightRadius={i === 8 ? "md" : "none"}
-          isActive={botStrength === i}
-          onClick={() => setBotStrength(i)}
-        >
-          {i}
-        </Button>
-      );
-    }
-    return (
-      <ButtonGroup spacing="0" size="xs">
-        {buttons}
-      </ButtonGroup>
-    );
-  };
+  const wrapWithGridItem = (components) =>
+    components.map((component) => (
+      <Grid item xs={4}>
+        {component}
+      </Grid>
+    ));
 
   return (
     <Box>
       <Container>
-        {/* Container Atas */}
-        {/* Container Atas */}
-        <Grid
-          // border="1px"
-          borderColor="gray.800"
-          borderRadius="lg"
-          templateRows="repeat(2, 3fr)"
-          templateColumns="repeat(2, 5fr)"
-          border="1px"
-          gap={0}
-        >
-          <GridItem w="100%" h="10" bg="transparent" colSpan={2}>
-            <Center>
-              <Text fontSize={20}>Play With</Text>
-            </Center>
-          </GridItem>
-
-          <GridItem
-            w="100%"
-            h="10"
-            bg="transparent"
-            border="1px"
-            borderBottomLeftRadius="lg"
-            marginBottom="0%"
-          >
-            <Center>
-              <Button variant="ghost" w="99%">
-                Bot
-              </Button>
-            </Center>
-          </GridItem>
-
-          <GridItem
-            w="100%"
-            h="10"
-            bg="transparent"
-            border="1px"
-            borderBottomRightRadius="lg"
-            marginBottom="0%"
-          >
-            <Center>
-              <Button variant="ghost" w="99%">
-                Human
-              </Button>
-            </Center>
-          </GridItem>
-        </Grid>
-
         {/* Container Bawah */}
-        <Grid
-          // border="1px"
-          borderColor="gray.800"
-          borderRadius="lg"
-          templateRows="repeat(2, 3fr)"
-          templateColumns="repeat(3, 5fr)"
-          gap={0}
-        >
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>5+3</Text>
-            </Center>
-          </Button>
-
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>10+0</Text>
-            </Center>
-          </Button>
-
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>10+5</Text>
-            </Center>
-          </Button>
-
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>15+10</Text>
-            </Center>
-          </Button>
-
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>30+0</Text>
-            </Center>
-          </Button>
-
-          <Button
-            px="2rem"
-            py="3rem"
-            bgColor="blue.200"
-            marginBottom="5%"
-            marginTop="10%"
-            marginLeft="10%"
-            marginRight="10%"
-            border="1px"
-            borderRadius="lg"
-          >
-            <Center>
-              <Text fontSize={25}>30+20</Text>
-            </Center>
-          </Button>
+        <Grid container spacing={2}>
+          {wrapWithGridItem(timeControlButtons)}
         </Grid>
       </Container>
     </Box>
