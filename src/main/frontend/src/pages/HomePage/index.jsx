06127ee0ef92bcb80c1185791595bca4ac955ae2
MarcelJulian <marceljulian2047@gmail.com>
2022-02-13 22:04:05 +0700

    refactor: :recycle: refactor into mui

--- src/main/frontend/src/pages/HomePage/index.jsx
+++ src/main/frontend/src/pages/HomePage/index.jsx
@@ -1,35 +1,65 @@
 import React, { useState } from "react";
 
-import {
-  Button,
-  ButtonGroup,
-  Container,
-  Grid,
-  GridItem,
-  Slider,
-  Flex,
-  // Spacer,
-  Center,
-  Text,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  Stack,
-  Switch
-} from "@chakra-ui/react";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Box from "@mui/material/Box";
-import MaterialButton from "@mui/material/Button";
-import MaterialButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import { amber, deepOrange, grey } from "@mui/material/colors";
-import MaterialContainer from "@mui/material/Container";
-import MaterialGrid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Slider from "@mui/material/Slider";
 import { ThemeProvider, useTheme, createTheme } from "@mui/material/styles";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Typography from "@mui/material/Typography";
+
+function SectionBox(props) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      sx={{ paddingX: "1rem", paddingY: "0.5rem" }}
+    >
+      {props.children}
+    </Box>
+  );
+}
+
+function SectionBoxTitle({ label }) {
+  return (
+    <Typography variant="h5" sx={{ marginBottom: "0.5rem" }}>
+      {label}
+    </Typography>
+  );
+}
+
+function BotColorButtonGroup({ botColor, handleBotColorChange }) {
+  return (
+    <ToggleButtonGroup
+      value={botColor}
+      exclusive
+      onChange={handleBotColorChange}
+      size="large"
+      sx={{ border: 1 }}
+    >
+      <ToggleButton value="white" disableFocusRipple disableRipple>
+        <Typography variant="button" sx={{ paddingX: "0.5rem" }}>
+          white
+        </Typography>
+      </ToggleButton>
+      <ToggleButton value="black" disableFocusRipple disableRipple>
+        <Typography variant="button" sx={{ paddingX: "0.5rem" }}>
+          black
+        </Typography>
+      </ToggleButton>
+      <ToggleButton value="random" disableFocusRipple disableRipple>
+        <Typography variant="button" sx={{ paddingX: "0.5rem" }}>
+          random
+        </Typography>
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
 
 function HomePage() {
   // replace with store
@@ -40,13 +70,17 @@ function HomePage() {
   const [botTimeControlMinute, setBotTimeControlMinute] = useState(10);
   const [botTimeControlIncrement, setBotTimeControlIncrement] = useState(0);
 
-  const handleBotStrengthChange = (_, strength) => {
-    setBotStrength(strength);
-  };
+  const handleBotStrengthChange = (_, strength) => setBotStrength(strength);
 
-  const handleBotTimeControlModeChange = (_, mode) => {
+  const handleBotColorChange = (_, color) => setBotColor(color);
+
+  const handleBotTimeControlModeChange = (_, mode) =>
     setBotTimeControlMode(mode);
-  };
+
+  const handleBotTimeControlMinuteChange = (_, minute) =>
+    setBotTimeControlMinute(minute);
+  const handleBotTimeControlIncrementChange = (_, increment) =>
+    setBotTimeControlIncrement(increment);
 
   const generateBotStrengthGroupButton = () => {
     const buttons = [];
@@ -75,7 +109,7 @@ function HomePage() {
 
   return (
     <Box sx={{ marginTop: "5%" }}>
-      <MaterialContainer>
+      <Container>
         {/* Container Atas */}
         <Card elevation={1} sx={{ marginBottom: "2rem" }}>
           <Box
@@ -120,7 +154,7 @@ function HomePage() {
                   // height: "100%"
                 }}
               >
-                <MaterialButton variant="ghost" sx={{ width: "100%" }}>
+                <Button variant="ghost" sx={{ width: "100%" }}>
                   <Typography
                     variant="h5"
                     color="primary"
@@ -128,13 +162,13 @@ function HomePage() {
                   >
                     Bot
                   </Typography>
-                </MaterialButton>
+                </Button>
               </Box>
-              <MaterialButton variant="ghost" sx={{ width: "100%" }}>
+              <Button variant="ghost" sx={{ width: "100%" }}>
                 <Typography variant="h5" color="primary">
                   Human
                 </Typography>
-              </MaterialButton>
+              </Button>
             </Box>
           </Box>
         </Card>
@@ -143,136 +177,73 @@ function HomePage() {
         <Card sx={{ marginBottom: "1rem" }}>
           <Box display="flex" flexDirection="column">
             {/* Box Strength Level AI */}
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              sx={{ paddingX: "1rem", paddingY: "0.5rem" }}
-            >
-              <Typography variant="h5" sx={{ marginBottom: "0.5rem" }}>
-                Strength
-              </Typography>
+            <SectionBox>
+              <SectionBoxTitle label="Strength" />
               {generateBotStrengthGroupButton()}
-            </Box>
+            </SectionBox>
             {/* Box color */}
-            <Box w="100%" py="0.5rem">
-              <Center mb="0.5rem">Color</Center>
-              <Flex justifyContent="space-evenly">
-                <Button
-                  bgColor="black"
-                  border="2px"
-                  size="xs"
-                  borderRadius="100%"
-                  m="0"
-                  isActive={botColor === "black"}
-                  onClick={() => {
-                    setBotColor("black");
-                  }}
-                  _hover={{
-                    BoxShadow:
-                      "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)"
-                  }}
-                  _active={{}}
-                />
-                <Button
-                  bgColor="white"
-                  border="2px"
-                  size="xs"
-                  borderRadius="100%"
-                  py="0"
-                  m="0"
-                  isActive={botColor === "white"}
-                  onClick={() => {
-                    setBotColor("white");
-                  }}
-                  _hover={{
-                    BoxShadow:
-                      "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)"
-                  }}
-                  _active={{}}
-                />
-                <Button
-                  bgColor="gray"
-                  border="2px"
-                  size="xs"
-                  borderRadius="100%"
-                  m="0"
-                  isActive={botColor === "random"}
-                  onClick={() => {
-                    setBotColor("random");
-                  }}
-                  _hover={{
-                    BoxShadow:
-                      "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)"
-                  }}
-                  _active={{}}
-                />
-              </Flex>
-            </Box>
+            <SectionBox>
+              <SectionBoxTitle label="Color" />
+              <BotColorButtonGroup
+                botColor={botColor}
+                handleBotColorChange={handleBotColorChange}
+              />
+            </SectionBox>
             {/* Box Time Control */}
-            <Box sx={{ display: "flex", width: "100%", padding: "1rem" }}>
-              <Box sx={{ mx: "auto" }}>Time Control</Box>
+            <SectionBox>
+              <SectionBoxTitle label="Time Control" />
               <ToggleButtonGroup
                 color="primary"
                 value={botTimeControlMode}
                 exclusive
                 onChange={handleBotTimeControlModeChange}
               >
-                <ToggleButton value="unlimited">Unlimited</ToggleButton>
-                <ToggleButton value="realtime">Real Time</ToggleButton>
+                <ToggleButton value="unlimited">
+                  <Typography variant="button" sx={{ paddingX: "0.5rem" }}>
+                    Unlimited
+                  </Typography>
+                </ToggleButton>
+                <ToggleButton value="realtime">
+                  <Typography variant="button" sx={{ paddingX: "0.5rem" }}>
+                    Real Time
+                  </Typography>
+                </ToggleButton>
               </ToggleButtonGroup>
-            </Box>
-            {/* Box Toggle */}
-            <Box w="100%" h="5" bg="transparent" py="1rem">
-              <Grid templateColumns="repeat(3, 1fr)" gap={0}>
-                <GridItem w="100%" h="10" bg="transparen">
-                  <Text fontSize={13}>Unlimited</Text>
-                </GridItem>
-                <GridItem w="100%" px="10px" h="10" bg="transparen">
-                  <Stack direction="row">
-                    <Switch colorScheme="teal" size="md" />
-                  </Stack>
-                </GridItem>
-                <GridItem w="100%" h="10" bg="transparen">
-                  <Text fontSize={13}>Real-Time</Text>
-                </GridItem>
-              </Grid>
-            </Box>
+            </SectionBox>
             {/* Box Minute Per Side */}
-            <Box w="100%" bg="transparent">
-              <Box px="1rem" py="0.5rem">
-                Minute Per Side :{" "}
-              </Box>
-              <Box paddingLeft={5} paddingRight={58}>
-                <Slider aria-label="slider-ex-1" defaultValue={10}>
-                  <SliderTrack>
-                    <SliderFilledTrack />
-                  </SliderTrack>
-                  <SliderThumb />
-                </Slider>
-              </Box>
-            </Box>
+            <SectionBox>
+              <SectionBoxTitle
+                label={`Minutes Per Side: ${botTimeControlMinute}`}
+              />
+              <Slider
+                value={botTimeControlMinute}
+                onChange={handleBotTimeControlMinuteChange}
+                max={180}
+              />
+            </SectionBox>
             {/* Box Minute Per Side */}
-            <Box w="100%" bg="transparent">
-              <Box py="0.5rem">Increment in second : </Box>
-              <Box paddingRight={5}>
-                <Slider aria-label="slider-ex-1" defaultValue={10}>
-                  <SliderTrack>
-                    <SliderFilledTrack />
-                  </SliderTrack>
-                  <SliderThumb />
-                </Slider>
-              </Box>
-            </Box>
+            <SectionBox>
+              <SectionBoxTitle
+                label={`Increment in Second: ${botTimeControlIncrement}`}
+              />
+              <Slider
+                value={botTimeControlIncrement}
+                onChange={handleBotTimeControlIncrementChange}
+                max={180}
+              />
+            </SectionBox>
             {/* Box START */}
-            <Box w="100%" h="10" bg="transparent" colSpan={2}>
-              <Center>
-                <Button colorScheme="blue">Start</Button>
-              </Center>
-            </Box>
+            <Button
+              variant="contained"
+              disabled={
+                botTimeControlMinute === 0 && botTimeControlIncrement === 0
+              }
+            >
+              Start
+            </Button>
           </Box>
         </Card>
-      </MaterialContainer>
+      </Container>
     </Box>
   );
 }
