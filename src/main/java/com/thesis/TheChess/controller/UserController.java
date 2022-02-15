15bf09473671196199de04d7534648033437d37a
choirunnisaptr <choirunnisappang@gmail.com>
2022-02-15 23:16:03 +0700

    translate endpoint /login dari js ke java

--- src/main/java/com/thesis/TheChess/controller/UserController.java
+++ src/main/java/com/thesis/TheChess/controller/UserController.java
@@ -1,37 +1,54 @@
 package com.thesis.TheChess.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-import com.thesis.TheChess.dto.LoginInput;
 import com.thesis.TheChess.dto.LoginOutput;
 import com.thesis.TheChess.service.UserService;
 
 @RestController
 @RequestMapping(path = "/")
 public class UserController {
-
 	@Autowired
 	UserService service;
 	
 	@GetMapping(path = "login-the-chess")
-	public ResponseEntity<LoginOutput> loginController(@RequestBody LoginInput data) throws Exception{
-		System.out.println("UserController - loginController - START - ");
+	public RedirectView loginController(HttpServletRequest request) throws Exception{
+		System.out.println("UserController - loginController START");
+		
+		RedirectView redirectView = null;
+
+		try {
+			redirectView = service.loginService(request);
+			
+			System.out.println("UserController - loginController END");
+			return redirectView;
+		} catch (Exception e) {
+			System.out.println("UserController - loginController ERROR");
+			return redirectView;
+		}
+	}
+	
+	@GetMapping(path = "callback-the-chess")
+	public ResponseEntity<LoginOutput> callbackController() throws Exception{
+		System.out.println("UserController - callbackController - START");
+		
+		LoginOutput output = null;
 		
 		try {
-			LoginOutput output = null;
-			output = service.loginService(data);
 			
-			System.out.println("UserController - loginController - END - ");
 			return ResponseEntity.status(HttpStatus.OK).body(output);
 		} catch (Exception e) {
-			System.out.println("UserController - loginController - ERROR - ");
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body();
+			System.out.println("UserController - callbackController - ERROR");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 		}
 	}
 }
--- src/main/java/com/thesis/TheChess/service/UserService.java
+++ src/main/java/com/thesis/TheChess/service/UserService.java
@@ -1,31 +1,108 @@
 package com.thesis.TheChess.service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import com.thesis.TheChess.dto.LoginInput;
-import com.thesis.TheChess.dto.LoginOutput;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Service
 public class UserService {
 
+	@Value("${client_id}")
+	private String client_id;
+
 	@Value("${lichess_url}")
     public String lichess_url;
-
-	public LoginOutput loginService(LoginInput input) throws Exception{
-		System.out.println("UserService - loginService - START - input >> " + input);
-		LoginOutput output = null;
+	
+	@Value("${theChess_url}")
+	private String chess_url;
+	
+	public RedirectView loginService(HttpServletRequest request) throws Exception{
+		System.out.println("UserService - loginService START - request >> " + request);
+		
+		RedirectView output = null;
 		
 		try {
-//			TODO:buat hit ke API lichess cb liat ke DownloadSPPJSPPK (dia gapake ResultServiceOutput)
+			String verifier = createVerifier();
+			String challenge = createChallenge(verifier);
 			
-			System.out.println("UserService - loginService - END - input >> " + input);
+			System.out.println("request.getSession().getAttributeNames() >> " + request.getSession().getAttributeNames());
+			
+//			req.session.codeVerifier = verifier
+			HttpSession session = request.getSession();
+			session.setAttribute("codeVerifier", verifier);
+			
+			String url = chess_url + "oauth?response_type=code&client_id=" + client_id + "&redirect_uri=" + chess_url + "&scope=board:play&code_challenge_method=S256&code_challenge=" + challenge;
+			
+			output = new RedirectView();
+			output.setUrl(url);
+			
+			System.out.println("UserService - loginService END - output: " + output);
 			return output;
 		} catch (Exception e) {
-			
-			System.out.println("UserService - loginService - ERROR - input >> " + input + " - Exception >> " + e.getMessage());
-			return null;
+			System.out.println("UserService - loginService ERROR - error: " + e.getMessage());
+			throw new Exception("ERROR loginService >> " + e.getMessage());
 		}
 	}
+
+//	const createChallenge = (verifier) => base64URLEncode(sha256(verifier));
+	private String createChallenge(String verifier) {
+		return base64URLEncode(sha256(verifier));
+	}
+
+//	const createVerifier = () => base64URLEncode(crypto.randomBytes(32));
+	private String createVerifier() throws NoSuchAlgorithmException {
+		/* Source: https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/ */
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+		byte[] salt = new byte[32];
+		sr.nextBytes(salt);
+		return base64URLEncode(salt.toString());
+	}
 	
+//	const sha256 = (buffer) => crypto.createHash('sha256').update(buffer).digest();
+	private String sha256(String salt) {
+		/* Source: https://howtodoinjava.com/java/java-security/how-to-generate-secure-password-hash-md5-sha-pbkdf2-bcrypt-examples/ */
+		String generatedPassword = null;
+		String passwordToHash = "password";
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt.getBytes());
+            
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 32).substring(1));
+            }
+            generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+
+	private String base64URLEncode(String str) {
+		return Base64.getEncoder().encodeToString(str.getBytes()).replace("+", "-").replace("/", "_").replace("=", "");
+	}
+
+	public void callbackService() throws Exception{
+		System.out.println("UserService - callbackService START");
+		
+		try {
+			
+			
+			System.out.println("UserService - callbackService END");
+		} catch (Exception e) {
+			System.out.println("UserService - callbackService ERROR - error >> " + e.getMessage());
+			throw new Exception("ERROR callbackService >> " + e.getMessage());
+		}
+	}
 }
