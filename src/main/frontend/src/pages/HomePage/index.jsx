9b79c7ffd36603fe98575c08db8ce7cb04a7d718
MarcelJulian <marceljulian2047@gmail.com>
2022-02-21 19:43:58 +0700

    refactor: :recycle: refactor cards to components

--- src/main/frontend/src/pages/HomePage/index.jsx
+++ src/main/frontend/src/pages/HomePage/index.jsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import { grey } from "@mui/material/colors";
 import Container from "@mui/material/Container";
 import { useSelector } from "react-redux";
 
@@ -14,57 +13,13 @@ import HumanCardContent from "./HumanCardContent";
 function HomePage() {
   // replace with store
   const [isLogin, setIsLogin] = useState(false);
-  // will be used for login service
-  const botForm = useSelector((state) => state.botForm);
-  const { timeControlMinute, timeControlIncrement } = botForm;
-  const isBotButtonDisabled =
-    timeControlMinute === 0 && timeControlIncrement === 0;
 
   return (
     <Box sx={{ marginTop: "1rem" }}>
       <Container>
         <Box display="flex" justifyContent="space-between">
-          <Card
-            sx={{
-              marginBottom: "1rem",
-              borderRadius: "16px",
-              width: "48%",
-              paddingTop: "1rem",
-              backgroundColor: "secondary"
-            }}
-          >
-            <CardTitle label="Play with Bot" />
-            <BotCardContent />
-            <Box display="flex">
-              <Button
-                variant="contained"
-                fullWidth
-                disabled={isBotButtonDisabled}
-              >
-                Start
-              </Button>
-            </Box>
-          </Card>
-          <Card
-            sx={{
-              marginBottom: "1rem",
-              borderRadius: "16px",
-              width: "48%",
-              backgroundColor: "secondary"
-            }}
-          >
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              sx={{ height: "100%", paddingTop: "1rem" }}
-            >
-              <CardTitle label="Play with Human" />
-              <Box display="flex" alignItems="center" sx={{ height: "100%" }}>
-                <HumanCardContent />
-              </Box>
-            </Box>
-          </Card>
+          <BotCard />
+          <HumanCard />
         </Box>
       </Container>
     </Box>
@@ -72,3 +27,56 @@ function HomePage() {
 }
 
 export default HomePage;
+
+function BotCard() {
+  // will be used for login service
+  const botForm = useSelector((state) => state.botForm);
+  const { timeControlMinute, timeControlIncrement } = botForm;
+  const isBotButtonDisabled =
+    timeControlMinute === 0 && timeControlIncrement === 0;
+
+  return (
+    <Card
+      sx={{
+        marginBottom: "1rem",
+        borderRadius: "16px",
+        width: "48%",
+        paddingTop: "1rem",
+        backgroundColor: "secondary"
+      }}
+    >
+      <CardTitle label="Play with Bot" />
+      <BotCardContent />
+      <Box display="flex">
+        <Button variant="contained" fullWidth disabled={isBotButtonDisabled}>
+          Start
+        </Button>
+      </Box>
+    </Card>
+  );
+}
+
+function HumanCard() {
+  return (
+    <Card
+      sx={{
+        marginBottom: "1rem",
+        borderRadius: "16px",
+        width: "48%",
+        backgroundColor: "secondary"
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        sx={{ height: "100%", paddingTop: "1rem" }}
+      >
+        <CardTitle label="Play with Human" />
+        <Box display="flex" alignItems="center" sx={{ height: "100%" }}>
+          <HumanCardContent />
+        </Box>
+      </Box>
+    </Card>
+  );
+}
