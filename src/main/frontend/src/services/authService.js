6e209d578b57320d6aa3c81aa26e888ea11ec77b
MarcelJulian <marceljulian2047@gmail.com>
2022-02-28 21:51:12 +0700

    fix: :bug: try open in a new tab

--- src/main/frontend/src/services/authService.js
+++ src/main/frontend/src/services/authService.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+  if (newWindow) newWindow.opener = null;
+};
+
 const signIn = async () => {
   try {
     // let url = "/api/apps";
@@ -7,7 +12,14 @@ const signIn = async () => {
     // harusnya post ???
     const response = await axios.get(url);
 
-    return { status: response.OutputSchema.Status };
+    console.log(
+      "🚀 ~ file: authService.js ~ line 9 ~ signIn ~ response",
+      response
+    );
+
+    openInNewTab(response.data);
+
+    return { status: response.Status };
   } catch (err) {
     return { status: err.response.OutputSchema.Status };
   }
