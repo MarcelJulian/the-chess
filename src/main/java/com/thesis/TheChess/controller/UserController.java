8260c491917c4fcdedf885bf104c7316f2dfcd33
choirunnisaptr <choirunnisappang@gmail.com>
2022-03-01 22:18:36 +0700

    update for callback service

--- src/main/java/com/thesis/TheChess/controller/UserController.java
+++ src/main/java/com/thesis/TheChess/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.thesis.TheChess.dto.CallbackOutput;
@@ -22,11 +23,14 @@ import com.thesis.TheChess.service.UserService;
 @RestController
 @RequestMapping(path = "/")
 public class UserController {
+	
+	@Value("${theChess_url}")
+	private String thechess_url;
+	
 	@Autowired
 	UserService service;
 	
 	@GetMapping(path = "login-the-chess")
-//	public ResponseEntity<Void> loginController(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 	public ResponseEntity<String> loginController(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		System.out.println("UserController - loginController START");
 		
@@ -42,9 +46,6 @@ public class UserController {
 			headers.add("Access-Control-Allow-Headers", "Content-Type");
 			headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 			
-//			response.sendRedirect(output);
-//			return ResponseEntity.status(HttpStatus.OK).headers(headers).body(null);
-//			return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).header(HttpHeaders.LOCATION, output).build();
 			return ResponseEntity.status(HttpStatus.OK).headers(headers).body(output);
 		} catch (Exception e) {
 			System.out.println("UserController - loginController ERROR - error >> " + e.getMessage());
@@ -58,6 +59,7 @@ public class UserController {
 		}
 	}
 	
+	/*
 	@GetMapping(path = "callback-the-chess")
 	public ResponseEntity<CallbackOutput> callbackController(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		System.out.println("UserController - callbackController - START");
@@ -82,6 +84,30 @@ public class UserController {
 			headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 			
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(headers).body(null);
+		}
+	}
+	*/
+	
+	@GetMapping(path = "callback-the-chess")
+	public RedirectView callbackController(HttpServletRequest request, HttpServletResponse response, RedirectAttributes ra) throws Exception{
+		System.out.println("UserController - callbackController - START");
+		
+		RedirectView redirectView = null;
+		CallbackOutput output = null;
+		
+		try {
+			output = service.callbackService(request);
+			
+			ra.addAttribute("access-token", output.getToken());
+			ra.addAttribute("username", output.getUsername());
+			
+			redirectView = new RedirectView();
+			redirectView.setUrl(thechess_url);
+			
+			return redirectView;
+		} catch (Exception e) {
+			System.out.println("UserController - callbackController - ERROR - error >> " + e.getMessage());
+			return redirectView;			
 		}
 	}
 }
