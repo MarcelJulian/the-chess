77874fb28881fb9f582428230ca5ef8a66b22c26
choirunnisaptr <choirunnisappang@gmail.com>
2022-03-17 22:53:12 +0700

    add service for make a board move

--- src/main/java/com/thesis/TheChess/controller/GamePlayController.java
+++ src/main/java/com/thesis/TheChess/controller/GamePlayController.java
@@ -1,10 +1,37 @@
 package com.thesis.TheChess.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thesis.TheChess.service.GamePlayService;
 
 @RestController
 @RequestMapping(path = "/")
 public class GamePlayController {
+	
+	@Autowired
+	GamePlayService service;
 
+	@PostMapping(path = "api/move-piece/{game_id}/{move}")
+	public ResponseEntity<Boolean> movePieceController(@RequestHeader("oauth") String user_oauth, @PathVariable String game_id, @PathVariable String move){
+		System.out.println("GamePlayController - movePieceController START - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+		
+		Boolean output = false;
+		
+		try {
+			output = service.movePieceService(user_oauth, game_id, move);
+			
+			System.out.println("GamePlayController - movePieceController END - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+			return ResponseEntity.status(HttpStatus.OK).body(output);
+		} catch (Exception e) {
+			System.out.println("GamePlayController - movePieceController ERROR - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move + " - exception >> " + e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(output);
+		}
+	}
 }
--- src/main/java/com/thesis/TheChess/service/GamePlayService.java
+++ src/main/java/com/thesis/TheChess/service/GamePlayService.java
@@ -1,8 +1,74 @@
 package com.thesis.TheChess.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.thesis.TheChess.dto.MakeBoardMoveResult;
 
 @Service
 public class GamePlayService {
+	
+	@Value("${lichess_api_url}")	//	https://lichess.org/api/
+    public String lichess_api_url;
+	
+	RestTemplate restTemplate = new RestTemplate();
 
+	public Boolean movePieceService(String user_oauth, String game_id, String move) throws Exception{
+		System.out.println("GamePlayService - movePieceService - START - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+		
+		boolean result = false;
+		
+		try {
+			result = hitMakeABoardMove(user_oauth, game_id, move);
+			
+			System.out.println("GamePlayService - movePieceService - END - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+			return result;
+		} catch (Exception e) {
+			System.out.println("GamePlayService - movePieceService - ERROR - user_oauth >> " + user_oauth  + " - game_id >> " + game_id + " - move >> " + move + " - exception >> " + e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+	}
+
+	private boolean hitMakeABoardMove(String user_oauth, String game_id, String move) throws Exception{
+		System.out.println("hitMakeABoardMove - START - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+		
+		boolean result = false;
+		
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Authorization", "Bearer " + user_oauth);
+			
+			HttpEntity request = new HttpEntity("", headers);
+			System.out.println("hitMakeABoardMove - request >> " + request);
+			
+			String uri = lichess_api_url + "board/game/" + game_id + "/move" + move;
+			System.out.println("hitAbortGame - uri >> " + uri);
+			
+			ResponseEntity<MakeBoardMoveResult> responseHit = restTemplate.exchange(uri, HttpMethod.POST, request, MakeBoardMoveResult.class);
+			System.out.println("hitChallengeAI - responseHit >> " + responseHit);
+			
+			if (responseHit.getStatusCodeValue() == 200) {
+				MakeBoardMoveResult output = responseHit.getBody();
+				
+				if (output.getOk().equalsIgnoreCase("true")){
+					result = true;
+				}
+			} else {
+				throw new Exception("Failed Make A Board Move; error code value >> " + responseHit.getStatusCodeValue());
+			}
+
+			System.out.println("hitMakeABoardMove - END - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move);
+			return result;
+		} catch (Exception e) {
+			System.out.println("hitMakeABoardMove - ERROR - user_oauth >> " + user_oauth + " - game_id >> " + game_id + " - move >> " + move + " - exception >> " + e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	
 }
